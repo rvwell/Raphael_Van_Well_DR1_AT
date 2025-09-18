15aa3c29d46abf7609fc20eacbf008c4e1ff3316
Raphael Van Well <rapha.well@outlook.com>
2025-09-18 15:54:11 -0300

    first

---
+++ src/main/java/com/infnet/dr1/at/AtApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.dr1.at;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infnet/dr1/at/AtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.dr1.at;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
